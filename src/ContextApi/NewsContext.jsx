36d78f538b43ec92e9ec31bde1ca02775c201599
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-01-30 01:26:58 +0500

    feat: fetch and display news data in card using Context API and Axios

--- src/ContextApi/NewsContext.jsx
+++ src/ContextApi/NewsContext.jsx
@@ -1,10 +1,33 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import api from "../Config/Axios";
 
 export const NewsContext = createContext()
 
 export const NewsContextProvider = ({children}) => {
+
+    const [news , setNews] = useState([])
+
+
+    const fetchData = async (url = "/everything?q=tesla") => {
+       try {
+        const res = await api.get(
+          `${url}&sortBy=publishedAt&apiKey=${import.meta.env.VITE_API_KEY}`,
+        );
+        return res.data;
+      }
+     catch (error) {
+        console.log(error)
+    }
+}
+
+    const value = {
+      news,
+      setNews,
+      fetchData,
+    };
+
     return (
-        <NewsContext.Provider value={'Qasim'}>
+        <NewsContext.Provider value={value}>
             {children}
         </NewsContext.Provider>
     )
--- src/Page/News.jsx
+++ src/Page/News.jsx
@@ -1,67 +1,47 @@
-import React, { useEffect } from 'react'
-import Wrapper from '../Component/Wrapper'
-import api from '../Config/Axios';
-import { useNewsContext } from '../ContextApi/NewsContext';
+import React, { useEffect } from "react";
+import Wrapper from "../Component/Wrapper";
+import { useNewsContext } from "../ContextApi/NewsContext";
 
-const News = ({className}) => {
+const News = ({ className }) => {
+  const { news, setNews, fetchData } = useNewsContext();
+  console.log(news);
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetchData();
+      setNews(data.articles);
+    })();
+  }, []);
+
   return (
     <Wrapper>
       <div className={`grid grid-cols-4 py-3 gap-6 bg-[#0e131b] ${className}`}>
-        <NewsCard />
-        {/* <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard /> */}
+        {news.map((newsDetails, index) => {
+          return  <NewsCard key={index} newsDetails={newsDetails} />;
+        })}
       </div>
     </Wrapper>
   );
-}
+};
 
-// 0758b6f658254f1b822e62d6c2413ddf
-
-const NewsCard = () => {
-
-   const state = useNewsContext();
-   console.log(state);
-
-  const fetchData = async () => {
-    const res = await api.get(
-         `/everything?q=tesla&from=2025-12-28&sortBy=publishedAt&apiKey=${import.meta.env.VITE_API_KEY}`
-    )
-    console.log(res.data)
-  }
-
-
-
-  useEffect(() => {
-    fetchData()
-  },[])
-
-    return (
-      <div className="card bg-black/20 text-white shadow-sm">
-        <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Card Title</h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
+const NewsCard = ({newsDetails}) => {
+  return (
+    <div className="card bg-black/20 text-white shadow-sm">
+      <figure>
+        <img src={newsDetails.urlToImage} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{newsDetails.title}</h2>
+        <p>
+          A card component has a figure, a body part, and inside body there are
+          title and actions parts
+        </p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default News
+export default News;
