ab4d396e1dd4526333706e2db1ead48658568b3d
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-01-30 22:08:06 +0500

    feat: fetch news dynamically on category selection - update global news state via Context API

--- src/Component/Catorgy.jsx
+++ src/Component/Catorgy.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Wrapper from "./Wrapper";
+import { useNewsContext } from "../ContextApi/NewsContext";
 
 const Catorgy = ({className}) => {
+
+  const {setNews, fetchData} = useNewsContext()
+
   const categories = [
     "business",
     "entertainment",
@@ -11,14 +15,26 @@ const Catorgy = ({className}) => {
     "sports",
     "technology",
   ];
+
+  const handleClick = async(e) => {
+      let cat = e.target.value;
+      let data = await fetchData(`/everything?q=${cat}`);
+       setNews(data.articles);
+    }
+
   return (
     <div className={`bg-[#0e131b] ${className}`}>
       <Wrapper>
         <div className="max-w-full w-fit flex m-auto overflow-x-auto scroll-none px-4 gap-5">
-          {categories.map((curItems, index) => {
+          {categories.map((category, index) => {
             return (
-              <button key={index} className="btn btn-primary">
-                {curItems}
+              <button
+                key={index}
+                value={category}
+                className="btn btn-primary"
+                onClick={handleClick}
+              >
+                {category}
               </button>
             );
           })}
